a88e898e502ca725f6648b6208b3a7b075d6ec3c
phantomics <a@imagegardenphoto.com>
2018-06-03 03:52:58 -0700

    more work on document view

--- seed.app-model.document-slate.base/README.txt
+++
@@ -1,1 +0,0 @@
-This is the stub README.txt for the "seed.app-model.document-slate.base" project.
--- seed.app-model.document.slate.base/package.lisp
+++
@@ -1,6 +0,0 @@
-;;;; package.lisp
-
-(defpackage #:seed.app-model.document.slate.base
-  (:export #:document-as-html)
-  (:use #:cl #:cl-who))
-
