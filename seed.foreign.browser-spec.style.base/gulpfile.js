46fc4c20adc5c7bcc8902e7683cec2bdff8ed7f6
phantomics <a@imagegardenphoto.com>
2018-07-04 06:43:46 -0700

    drag and drop functions

--- seed.foreign.browser-spec.style.base/gulpfile.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    var gulp = require('gulp');
-    var concat = require('gulp-concat');
-    return gulp.task('dev', function () {
-        return gulp.src(['/Users/owner/Documents/src/seed/seed.foreign.browser-spec.style.base/node_modules/bootstrap/dist/css/bootstrap.min.css', '/Users/owner/Documents/src/seed/seed.foreign.browser-spec.style.base/node_modules/react-select/dist/react-select.css', '/Users/owner/Documents/src/seed/seed.foreign.browser-spec.style.base/node_modules/codemirror/lib/codemirror.css', '/Users/owner/Documents/src/seed/seed.foreign.browser-spec.style.base/node_modules/codemirror/theme/solarized.css']).pipe(concat('main.css')).pipe(gulp.dest('/Users/owner/Documents/src/seed/portal.demo1/'));
-    });
-})();
--- seedFS2/package.lisp
+++
@@ -1,6 +0,0 @@
-;;;; package.lisp
-
-(defpackage #:seed.foreign.browser-spec.script.base
-  (:export)
-  (:use #:cl #:parenscript))
-
