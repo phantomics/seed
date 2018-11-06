df2133c637711170a9a6166bfbc83d7a3c19d68c
phantomics <a@imagegardenphoto.com>
2018-11-06 10:56:34 -0800

    graph and site form improvements

--- seed.app-model.site.base/site.base.lisp
+++ seed.app-model.site.base/site.base.lisp
@@ -10,14 +10,31 @@
 		       (let* ((element (j-query data))
 			      (interaction-data (chain element (attr "interaction")
 						       (split " "))))
-			 (chain console (log 9091 element interaction-data
-					     data
-					     (chain element (find "button.submit"))))
+			 ;; (chain console (log 9091 element interaction-data
+			 ;; 		     data
+			 ;; 		     (chain element (find "button.submit"))))
 			 (chain (chain element (find "button.submit"))
 				(on "click" (lambda (event)
 					      (chain event (prevent-default))
-					      (chain console (log 515
-								  (chain element (serialize-j-s-o-n)))))))))))))
+					      (chain j-query
+						     (ajax (create url "../portal"
+								   type "POST"
+								   data-type "json"
+								   content-type "application/json; charset=utf-8"
+								   data (chain -j-s-o-n
+									       (stringify
+										(chain interaction-data
+										       (concat
+											(chain
+											 element
+											 (serialize-j-s-o-n))))))
+								   success (lambda (data)
+									     (chain console (log :returned data))
+									     ;; (callback stage-data data)
+									     )
+								   error (lambda (data err)
+									   (chain console (log 11 data err)))
+								   ))))))))))))
 
 (defvar form-support-effects)
 (setq form-support-effects
